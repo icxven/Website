65af84e3dab14dcc2903a84cbdc18a74c48ea058
icxven <icxven@gmail.com>
2023-07-02 21:10:20 +0530

    Simple Fix meta.image missing fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,7 +14,6 @@ export default function Document() {
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@icxven" />
         <meta name="twitter:title" content={meta.title} />
